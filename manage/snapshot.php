0b64f22977f7030280ffd5decad13488e9216a58
Bret Hudson <bret@brethudson.com>
2017-07-21 20:32:21 -0500

    Snapshot page has been modified for showing score trends. Also cleaned up the JS a bit.

--- manage/snapshot.php
+++ manage/snapshot.php
@@ -25,25 +25,29 @@
 	
 	<script type="text/javascript">
 		var defaultScoreDivClass = 'score-circle';
+		var defaultScoreTrendClass = 'trend-icon';
 		var snapshots = [];
+		var numScoresPerCol = 2;
 		
-		function createScorePair(score, div, type) {
+		function createScorePair(score, scoreDiv, trendDiv, type) {
 			return {
 				score: score, // TODO(bret): Make this per game
-				div: div,
+				scoreDiv: scoreDiv,
+				trendDiv: trendDiv,
 				type: type,
 			};
 		}
 		
 		Snapshot = function(data) {
 			var $this = this;
+			this.data = data;
 			
 			this.scoreInfo = [
-				createScorePair(0, null, 'Average'),
+				//createScorePair(0, null, 'Average'),
 				createScorePair(0, null, 'Points/Second'),
 				createScorePair(0, null, 'Accuracy'),
 				
-				createScorePair(0, null, 'Average'),
+				//createScorePair(0, null, 'Average'),
 				createScorePair(0, null, 'Points/Second'),
 				createScorePair(0, null, 'Accuracy'),
 			];
@@ -56,6 +60,7 @@
 			nameCol = $('<div class="col-xs-3"><span class="name">' + data.name + '</span></div>');
 			parentRow.append(nameCol);
 			
+			// NOTE(bret): We probably don't need this since we're not sending data back, but we may end up needing it anyway.
 			/*<input id="username-' + data.username + '" type="hidden" name="username" value="' + data.username + '" />*/
 			
 			for (var colIndex = 0; colIndex < 2; ++colIndex) {
@@ -68,49 +73,92 @@
 				scorePairRow = $('<div class="row"></div>');
 				scorePair.append(scorePairRow);
 				
-				for (var scoreIndex = 0; scoreIndex < 3; ++scoreIndex) {
-					var col = $('<div class="col-xs-4"></div>');
-					var scoreDiv = $('<div class="' + defaultScoreDivClass + '" data-toggle="tooltip" title="XX%"></div>');
-					col.append(scoreDiv);
+				var colSize = 12 / numScoresPerCol;
+				var col, scoreDiv, trendDiv;
+				for (var scoreIndex = 0; scoreIndex < numScoresPerCol; ++scoreIndex) {
+					col = $('<div class="col-xs-' + colSize + '"></div>');
+					
+					scoreDiv = $('<span class="' + defaultScoreDivClass + '" data-toggle="tooltip" title="XX%"></span>');
 					scoreDiv.tipsy({ gravity: 's', html: true });
-					$this.scoreInfo[(colIndex * 3) + scoreIndex].div = scoreDiv;
+					col.append(scoreDiv);
+					
+					trendDiv = $('<span class="' + defaultScoreTrendClass + '"></span>');
+					col.append(trendDiv);
+					
 					scorePairRow.append(col);
+					
+					$this.scoreInfo[(colIndex * numScoresPerCol) + scoreIndex].scoreDiv = scoreDiv;
+					$this.scoreInfo[(colIndex * numScoresPerCol) + scoreIndex].trendDiv = trendDiv;
 				}
 			}
 			
-			scores = [
-				Math.random(),
-				Math.random(),
-				Math.random(),
-				Math.random(),
-				Math.random(),
-				Math.random()
-			];
+			scores = [];
+			for (var i = 0; i < numScoresPerCol * 2; ++i) {
+				scores.push(Math.random());
+			}
+			this.normalizeScores(scores);
 			this.updateScores(scores);
+			this.updateTrends();
 			
 			this.div = parentDiv;
 			$("#snapshots").append(parentDiv);
 		}
 		
+		Snapshot.prototype.normalizeScores = function(scores) {
+			for (var i = 0; i < numScoresPerCol * 2; ++i) {
+				//
+			}
+		}
+		
+		// TODO(bret): Find a better, cleaner way to write all this, jeesh
 		Snapshot.prototype.updateScores = function(scores) {
-			for (var s = 0; s < 6; ++s) {
+			var scoreDiv;
+			var classToUse;
+			for (var s = 0; s < numScoresPerCol * 2; ++s) {
+				scoreDiv = this.scoreInfo[s].scoreDiv;
+				
 				this.scoreInfo[s].score = scores[s];
-				this.scoreInfo[s].div.attr('class', defaultScoreDivClass);
+				classToUse = defaultScoreDivClass;
 				
 				var percent = (scores[s] * 100).toFixed(2);
-				var type = this.scoreInfo[s].type + ((s < 3) ? ' (Summary)' : ' (Last Game)');
-				this.scoreInfo[s].div.attr('title', type + '<br />' + percent + '%');
+				var type = this.scoreInfo[s].type + ((s < numScoresPerCol) ? ' (Summary)' : ' (Last Game)');
+				scoreDiv.attr('title', type + '<br />' + percent + '%');
 				
-				if (scores[s] >= 0.7)
-					this.scoreInfo[s].div.addClass('green');
-				else if (scores[s] < 0.5)
-					this.scoreInfo[s].div.addClass('red');
+				// TODO(bret): Make these use actual numbers
+				if (scores[s] >= 0.7) {
+					classToUse += ' green icon-circle';
+				} else if (scores[s] < 0.5) {
+					classToUse += ' red icon-circle';
+				} else {
+					classToUse += ' icon-circle-empty';
+				}
+				
+				scoreDiv.attr('class', classToUse);
+			}
+		}
+		
+		Snapshot.prototype.updateTrends = function() {
+			var trendDiv;
+			var noTrendIcon = 'icon-minus'; // NOTE(bret): Feel free to change this to 'empty'
+			var classToUse;
+			for (var s = 0; s < numScoresPerCol * 2; ++s) {
+				trendDiv = this.scoreInfo[s].trendDiv;
+				classToUse = defaultScoreTrendClass;
+				
+				switch (Math.floor(Math.random() * 3)) {
+					case 0: classToUse += ' green icon-up-dir'; break;
+					case 1: classToUse += ' red icon-down-dir'; break;
+					case 2: classToUse += ' icon-minus'; break;
+				}
+				
+				trendDiv.attr('class', classToUse);
 			}
 		}
 		
 		$(window).ready(function() {
 			snapshots[1] = new Snapshot({ id: 1, name: 'George' });
-			snapshots[2] = new Snapshot({ id: 2, name: 'Anthony' });
+			snapshots[2] = new Snapshot({ id: 2, name: 'Lucas' });
+			snapshots[3] = new Snapshot({ id: 3, name: 'Ian' });
 		});
 	</script>
 	<!-- <script src="<?php echo $documentroot; ?>js/manage/users.js"></script> -->
@@ -141,18 +189,6 @@
                                 <option value="128">Facts Assessment</option>
 							</select>
 						</div>
-						<!--<div class="snapshot">
-							<span class="name">Name (Games)</span>
-							<?php
-								for ($i = 0; $i < 2; ++$i) {
-							?>
-							<div class="scores-3-pair">
-								<div class="score-title">Avg</div>
-								<div class="score-title">Pts/<br/>Sec</div>
-								<div class="score-title">Accu.</div>
-							</div>
-							<?php } ?>
-						</div>-->
 						<div class="snapshot">
 							<div class="row">
 								<div class="col-xs-3">
@@ -170,13 +206,13 @@
 								<div class="col-xs-4">
 									<div class="scores-3-pair">
 										<div class="row">
-											<div class="col-xs-4">
+											<!--<div class="col-xs-4">
 												<div class="score-title" data-toggle="tooltip"title="Average">Avg</div>
-											</div>
-											<div class="col-xs-4">
+											</div>-->
+											<div class="col-xs-6">
 												<div class="score-title" data-toggle="tooltip" title="Points Per Second">PPS</div>
 											</div>
-											<div class="col-xs-4">
+											<div class="col-xs-6">
 												<div class="score-title" data-toggle="tooltip" title="Accuracy">Acc</div>
 											</div>
 										</div>
@@ -185,26 +221,7 @@
 									<?php } ?>
 							</div>
 						</div>
-						<div id="snapshots">
-							<?php
-								/*function createStudent($lname, $fname, $username, $id) {
-									$name = $lname . ', ' . $fname;
-							?>
-							
-							<?php
-								}
-								
-								$students = $user->getStudents('ORDER BY Lname ASC, Fname ASC', '*', $groupName);
-								foreach ($students as &$student) {
-									$student['Lname'] = User::decode($student['Lname']);
-									$student['Fname'] = User::decode($student['Fname']);
-								}
-								$user->sortStudents($students);
-								foreach ($students as $s) {
-									createStudent($s['Lname'], $s['Fname'], $s['Username'], $s['Id']);
-								}*/
-							?>
-						</div>
+						<div id="snapshots"></div>
 						<div class="footer center">
 							End of students
 						</div>
